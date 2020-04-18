e5a8dcfb5a7927a4bedea01c5921f6f728194f94
rory-roccio <rory.roccio@gmail.com>
2020-04-18 12:27:11 -0700

    Add entry point

---
+++ App.tsx
@@ -0,0 +1,20 @@
+/* eslint-disable react/jsx-filename-extension */
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Ok, lets do it</Text>
+    </View>
+  );
+}
