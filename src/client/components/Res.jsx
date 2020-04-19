1da030ddbb22a10ac6fe89e15f3fb66f07b358c2
rory-roccio <rory.roccio@gmail.com>
2020-04-18 20:58:15 -0700

    Add text styles and Views for each resource

--- src/client/components/Res.jsx
+++ src/client/components/Res.jsx
@@ -4,16 +4,41 @@ import { StyleSheet, Text, View } from 'react-native';
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    flexDirection: 'row',
     backgroundColor: 'coral',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  resourceBox: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  header: {
+    color: 'seashell',
+    fontWeight: 'bold',
     justifyContent: 'center',
     alignItems: 'center',
   },
 });
 
-const Res = () => {
+const Res = ({ resources }) => {
+  const { woodCount, foodCount, goldCount, stoneCount } = resources;
+  const { container, resourceBox, header } = styles;
   return (
-    <View style={styles.container}>
-      <Text>Res</Text>
+    <View style={container}>
+      <View style={resourceBox}>
+        <Text style={header}>{`Wood\n  ${woodCount}`}</Text>
+      </View>
+      <View style={resourceBox}>
+        <Text style={header}>{`Food\n  ${foodCount}`}</Text>
+      </View>
+      <View style={resourceBox}>
+        <Text style={header}>{`Gold\n  ${goldCount}`}</Text>
+      </View>
+      <View style={resourceBox}>
+        <Text style={header}>{`Stone\n  ${stoneCount}`}</Text>
+      </View>
     </View>
   );
 };
