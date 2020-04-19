07b8f22ebc2a64628ddd1de70fa667a77e761af5
rory-roccio <rory.roccio@gmail.com>
2020-04-18 19:28:18 -0700

    Add time slider

--- src/client/components/Time.jsx
+++ src/client/components/Time.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Slider, Text, View } from 'react-native';
 
 const styles = StyleSheet.create({
   container: {
@@ -8,13 +8,19 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
   },
+  slider: {
+    width: 500,
+    maxWidth: '90%',
+  },
 });
 
 const Time = () => {
   const [time, setTime] = useState(0);
+  const maxTime = 1000;
   return (
     <View style={styles.container}>
-      <Text>Time</Text>
+      <Text>Game Time: {time}</Text>
+      <Slider style={styles.slider} minimumValue={0} maximum Value={maxTime} onValueChange={(value) => { setTime(parseInt(value * maxTime, 10)); }} />
     </View>
   );
 };
