02b3838ccff9604405babeeda7e666777b10fdd4
rory-roccio <rory.roccio@gmail.com>
2020-04-20 20:38:05 -0700

    Add middleware, GET building routes

--- server/index.js
+++ server/index.js
@@ -1,13 +1,43 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const { getBuildingUnits, getBuildingTechs, getGatherRates, postBuild } = require('./db.model.js');
+const cors = require('cors');
+const { getAllBuildings, getBuilding, getBuildingUnits, getBuildingTechs, getUnit, getGatherRates, postBuild } = require('./db.model.js');
 
 const app = express();
 const port = process.env.PORT || 3000;
 app.use(bodyParser());
+app.use(cors());
+
+
+// GET /buildings
+app.get('/buildings', (req, res) => {
+  console.log('GET /buildings');
+  getAllBuildings((err, results) => {
+    if (err) {
+      console.error(err);
+      res.status(400).send();
+    } else {
+      res.status(200).send(results);
+    }
+  });
+});
+
+// GET /buildings/:buildingId
+app.get('/buildings/:buildingId', (req, res) => {
+  console.log(`GET /buildings/${req.params.buildingId}`);
+  getBuilding(req.params.buildingId, (err, results) => {
+    if (err) {
+      console.error(err);
+      res.status(400).send();
+    } else {
+      res.status(200).send(results);
+    }
+  });
+});
 
 // GET /buildings/:buildingId/units
 app.get('/buildings/:buildingId/units', (req, res) => {
+  console.log(`GET /buildings/${req.params.buildingId}/units`);
   getBuildingUnits(req.params.buildingId, (err, results) => {
     if (err) {
       console.error(err);
@@ -20,7 +50,21 @@ app.get('/buildings/:buildingId/units', (req, res) => {
 
 // GET /buildings/:buildingId/techs
 app.get('/buildings/:buildingId/techs', (req, res) => {
+  console.log(`GET /buildings/${req.params.buildingId}/techs`);
   getBuildingTechs(req.params.buildingId, (err, results) => {
+    if (err) {
+      console.error(err);
+      res.status(400).send();
+    } else {
+      res.status(200).send(results);
+    }
+  });
+});
+
+// GET /units/:unitId
+app.get('/units/:unitId', (req, res) => {
+  console.log(`GET /units/${req.params.unitId}`);
+  getUnit(req.params.unitId, (err, results) => {
     if (err) {
       console.error(err);
       res.status(400).send();
@@ -32,6 +76,7 @@ app.get('/buildings/:buildingId/techs', (req, res) => {
 
 // GET /gatherRates
 app.get('/gatherRates', (req, res) => {
+  console.log('GET /gatherRates');
   getGatherRates((err, results) => {
     if (err) {
       console.error(err);
