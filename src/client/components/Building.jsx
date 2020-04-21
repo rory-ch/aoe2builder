3e6271bc2570e866670400bd14ef5b1e66ab6249
rory-roccio <rory.roccio@gmail.com>
2020-04-21 12:37:55 -0700

    Add style logic for incomplete buildings

--- src/client/components/Building.jsx
+++ src/client/components/Building.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import { StyleSheet, ActivityIndicator, Image } from 'react-native';
-import { Tooltip, Text } from 'react-native-elements';
+import { StyleSheet, ActivityIndicator, View, Image } from 'react-native';
+import { Tooltip } from 'react-native-elements';
 import Menu from './Menu.jsx';
 
 const styles = StyleSheet.create({
@@ -10,18 +10,32 @@ const styles = StyleSheet.create({
     borderRadius: 3,
     marginBottom: 4,
   },
+  iconTraining: {
+    width: 45,
+    height: 45,
+    borderRadius: 3,
+    marginBottom: 4,
+    opacity: 0.3,
+  },
   tooltip: {
     backgroundColor: 'rgba(0, 0, 0, 0)',
   },
 });
 
-const Building = ({ building, addUnit }) => {
+const Building = ({ building, addUnit, addTech, time }) => {
   const { image, units, techs } = building;
-  const { icon, tooltip } = styles;
+  const { icon, tooltip, iconTraining } = styles;
+  if (techs.length > 0 && building.completesAt <= time) {
+    return (
+      <Tooltip containerStyle={tooltip} overlayColor="rgba(6, 6, 6, 0.7)" withPointer={false} popover={<Menu units={units} techs={techs} addUnit={addUnit} addTech={addTech} time={time} />}>
+        <Image source={{ uri: image }} style={icon} PlaceholderContent={<ActivityIndicator />} />
+      </Tooltip>
+    );
+  }
   return (
-    <Tooltip containerStyle={tooltip} overlayColor="rgba(6, 6, 6, 0.7)" withPointer={false} popover={<Menu units={units} techs={techs} addUnit={addUnit} />}>
-      <Image source={{ uri: image }} style={icon} PlaceholderContent={<ActivityIndicator />} />
-    </Tooltip>
+    <View>
+      <Image source={{ uri: image }} style={building.completesAt > time ? iconTraining : icon} PlaceholderContent={<ActivityIndicator />} />
+    </View>
   );
 };
 
