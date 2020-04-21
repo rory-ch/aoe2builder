54935ab2a9265a24275043055610d516bd4e1da1
rory-roccio <rory.roccio@gmail.com>
2020-04-20 20:38:28 -0700

    Change App file extension to jsx

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import { registerRootComponent } from 'expo';
 
-import App from './src/client/App.tsx';
+import App from './src/client/App.jsx';
 
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in the Expo client or in a native build,
