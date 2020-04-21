09d1fe5b6b9af4e189d70d58bb5b8e2ff6aff16a
rory-roccio <rory.roccio@gmail.com>
2020-04-21 12:37:36 -0700

    Adjust styles logic to account for incomplete units

--- src/client/components/Unit.jsx
+++ src/client/components/Unit.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { StyleSheet, ActivityIndicator, Image, View } from 'react-native';
 import { Tooltip } from 'react-native-elements';
-import Menu from './Menu.jsx';
+import UnitMenu from './UnitMenu.jsx';
 
 const styles = StyleSheet.create({
   icon: {
@@ -10,25 +10,31 @@ const styles = StyleSheet.create({
     borderRadius: 3,
     marginBottom: 4,
   },
+  iconTraining: {
+    width: 45,
+    height: 45,
+    borderRadius: 3,
+    marginBottom: 4,
+    opacity: 0.3,
+  },
   tooltip: {
     backgroundColor: 'rgba(0, 0, 0, 0)',
   },
 });
 
-const Unit = ({ unit, tasks }) => {
+const Unit = ({ unit, tasks, time, addBuilding }) => {
   const { image } = unit;
-  const { icon, tooltip } = styles;
-
-  if (tasks.length > 1) {
+  const { icon, tooltip, iconTraining } = styles;
+  if (Object.keys(tasks).length > 0 && unit.completesAt <= time) {
     return (
-      <Tooltip containerStyle={tooltip} overlayColor="rgba(6, 6, 6, 0.7)" withPointer={false} popover={<Menu tasks={tasks} />}>
+      <Tooltip containerStyle={tooltip} overlayColor="rgba(6, 6, 6, 0.7)" withPointer={false} popover={<UnitMenu addBuilding={addBuilding} time={time} tasks={tasks} />}>
         <Image source={{ uri: image }} style={icon} PlaceholderContent={<ActivityIndicator />} />
       </Tooltip>
     );
   }
   return (
-    <View >
-      <Image source={{ uri: image }} style={icon} PlaceholderContent={<ActivityIndicator />} />
+    <View>
+      <Image source={{ uri: image }} style={unit.completesAt > time ? iconTraining : icon} PlaceholderContent={<ActivityIndicator />} />
     </View>
   );
 };
